af16336478f6020830fa69a9c68b4c066a5406a7
Surya K S <surya@Suryas-MacBook-Air.local>
2024-01-09 16:27:45 +0530

    tour of go part 1 (still needs some changes related to package)

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/suryxks/learning-go
+module learning-go
 
 go 1.21.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learning-go/basics/basics"
+)
+
+func main() {
+	fmt.Println("Hello, world")
+	basics.Basic()
+}
